a65fc54e1d435fd7d06b0a1aef88d13da978b3c8
Lucas7yoshi <Lucas7yoshi@gmail.com>
2019-06-17 22:09:53 -0400

    Add project files.

---
+++ UmodelAutoAes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Diagnostics;
+using System;
+
+namespace UmodelAutoAes
+{
+    class Program
+    {
+        const string aesurl = @"https://lucas7yoshi.github.io/aes.txt";
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            Console.WriteLine("Grabbing AES Key...");
+            var aes = wc.DownloadString(aesurl);
+            Process.Start($"umodel.exe", string.Join(" ", args) + " -aes=" + aes);
+        }
+    }
+}
